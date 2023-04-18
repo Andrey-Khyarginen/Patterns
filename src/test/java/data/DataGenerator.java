0b50bf1294bfd0a2966e5ff2c4f2d77a39251c2e
Andrey Kharginen <kut13@inbox.ru>
2023-04-18 17:29:48 +0400

    add code

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -1,6 +1,9 @@
 package data;
 
-public class DataGenerator {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 
 import com.github.javafaker.Faker;
 import lombok.Value;
@@ -15,27 +18,27 @@ import java.util.Random;
         private DataGenerator() {
         }
 
+        private static Faker faker;
+
         public static String generateDate(int shift) {
-            // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-            // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-            return date;
+            return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         }
 
         public static String generateCity(String locale) {
-            // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-            // с помощью Faker, либо используя массив валидных городов и класс Random
+            faker = new Faker(new Locale("ru"));
+            String city = faker.address().city();
             return city;
         }
 
         public static String generateName(String locale) {
-            // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-            // использовать Faker
+            faker = new Faker(new Locale("ru"));
+            String name = faker.name().fullName();
             return name;
         }
 
         public static String generatePhone(String locale) {
-            // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-            // использовать Faker
+            faker = new Faker(new Locale("ru"));
+            String phone = faker.phoneNumber().phoneNumber();
             return phone;
         }
 
@@ -44,9 +47,7 @@ import java.util.Random;
             }
 
             public static UserInfo generateUser(String locale) {
-                // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-                // generateName(locale), generatePhone(locale)
-                return user;
+                return new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
             }
         }
 
@@ -57,4 +58,4 @@ import java.util.Random;
             String phone;
         }
     }
-}
+
